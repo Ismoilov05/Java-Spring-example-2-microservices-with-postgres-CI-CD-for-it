7d0c4198a58fc111d3d0322f2a0d59e23635e01e
@ <oe@qodir.local>
2026-09-10 13:56:33 +0500

    spring boot, dockerfile files were created, docker container created

--- user-service/src/main/java/user_service/UserServiceApplication.java
+++ user-service/src/main/java/user_service/UserServiceApplication.java
@@ -1,3 +1,4 @@
+
 package user_service;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 }
---
+++ user-service/src/main/java/user_service/controller/UserController.java
@@ -0,0 +1,41 @@
+
+package user_service.controller;
+
+import user_service.entity.User;
+import user_service.service.UserService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
+
